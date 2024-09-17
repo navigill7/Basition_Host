cc5b4dc32f23282be16a782a8345842e95d6a727
navigill7 <nnavigill784@gmail.com>
2024-09-17 14:00:54 +0530

    configure route table for IGW

--- public_subnet.tf
+++ public_subnet.tf
@@ -3,6 +3,7 @@ resource "aws_subnet" "Basiton_Subnet_A1" {
   vpc_id = aws_vpc.Basiton_VPC.id
   cidr_block = var.subnet_A1_cidr
   availability_zone = var.availability_zone_A
+  map_public_ip_on_launch = true
   tags = {
     "Name": "Basiton_Subnet_A1"
   }
@@ -13,6 +14,7 @@ resource "aws_subnet" "Basiton_Subnet_B1" {
   vpc_id = aws_vpc.Basiton_VPC.id
   cidr_block = var.subnet_B1_cidr
   availability_zone = var.availability_zone_B
+  map_public_ip_on_launch = true
   tags = {
     "Name": "Basiton_Subnet_B1"
   }
--- route_table.tf
+++ route_table.tf
@@ -1,2 +1,23 @@
 # configuring the routes for internet gateway to public subnet in AZ A 
-  
+  resource "aws_route_table" "IGW_Route" {
+    vpc_id = aws_vpc.Basiton_VPC.id
+
+    route = {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.id
+    }
+
+    tags = {
+        Name = "TerraformPublicRT"
+    }
+  }
+
+  resource "aws_route_table_association" "subnet_association_IGW" {
+    subnet_id = aws_subnet.Basiton_Subnet_A1.id
+    route_table_id = aws_route_table.IGW_Route.id
+
+  }
+  resource "aws_route_table_association" "subnet_association_IGW_2" {
+    subnet_id = aws_subnet.Basiton_Subnet_B1.id
+    route_table_id = aws_route_table.IGW_Route.id
+  }
